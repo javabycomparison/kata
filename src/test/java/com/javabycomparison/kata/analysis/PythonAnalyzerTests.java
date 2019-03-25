4a2696e132e4a5923c5b9c83462786ecdd2d115f
Simon Harrer <simon.harrer@innoq.com>
2019-03-25 10:17:53 +0100

    update kata (automated commit)

--- src/test/java/com/javabycomparison/kata/analysis/PythonAnalyzerTests.java
+++ src/test/java/com/javabycomparison/kata/analysis/PythonAnalyzerTests.java
@@ -20,7 +20,13 @@ class PythonAnalyzerTests {
   void analyzePythonFile() {
     Assertions.assertEquals(
         pythonResult,
-        new ResultData(1, "./src/main/resources/python_files/function.py", 16, 3, 3, 0));
+        new ResultData(
+            1,
+            "./src/main/resources/python_files/function.py".replaceAll("\\\\", "/"),
+            16,
+            3,
+            3,
+            0));
   }
 
   @Test
